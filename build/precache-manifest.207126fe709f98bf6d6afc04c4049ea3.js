b582dfe7c5bea6d3ac7b625b61730e36a73c262e
ShravanKumarD <97720033+ShravanKumarD@users.noreply.github.com>
2024-05-06 18:36:04 +0530

    "faqs"

---
+++ build/precache-manifest.207126fe709f98bf6d6afc04c4049ea3.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28c7bcf0e6810e61cf33ddbdf284e8d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56a3ee44dba2f44fd1e5",
+    "url": "/static/css/main.c57c0f35.chunk.css"
+  },
+  {
+    "revision": "9af7fac7fabb5924c2e5",
+    "url": "/static/js/2.891e5bcc.chunk.js"
+  },
+  {
+    "revision": "10bf9094a5a2b4f874be543053665164",
+    "url": "/static/js/2.891e5bcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1591d4c0721408f08ae5",
+    "url": "/static/js/3.975bd232.chunk.js"
+  },
+  {
+    "revision": "56a3ee44dba2f44fd1e5",
+    "url": "/static/js/main.81ec052c.chunk.js"
+  },
+  {
+    "revision": "dde44d24cf8c1bd25724",
+    "url": "/static/js/runtime-main.cad17b8a.js"
+  },
+  {
+    "revision": "3d103dd14175b16a995e39152987a4d9",
+    "url": "/static/media/IMG1.3d103dd1.png"
+  },
+  {
+    "revision": "68acfa6b5f688871f277b686e635db22",
+    "url": "/static/media/IMG2.68acfa6b.png"
+  },
+  {
+    "revision": "a82454397fc07ce95523b7556e16b5d9",
+    "url": "/static/media/IMG3.a8245439.png"
+  },
+  {
+    "revision": "9dc75810407ebb3efbb30270af4c0cdc",
+    "url": "/static/media/IMG4.9dc75810.png"
+  },
+  {
+    "revision": "51a8addd052f5598a9657035446b13a7",
+    "url": "/static/media/IMG5.51a8addd.png"
+  },
+  {
+    "revision": "703c743fb169106d535312942e91d50f",
+    "url": "/static/media/Products_Page_Loan_steps.703c743f.png"
+  },
+  {
+    "revision": "f14e7ea632120cb93f8b895c0380a8e6",
+    "url": "/static/media/aboutus.f14e7ea6.png"
+  },
+  {
+    "revision": "fe8fc7992c938acc5fefaea85efc9998",
+    "url": "/static/media/agf.fe8fc799.jpeg"
+  },
+  {
+    "revision": "17299ea7f78e3e0f723a6ab149d57823",
+    "url": "/static/media/approvalIcon.17299ea7.png"
+  },
+  {
+    "revision": "199806bef1ca21620b6b815f6f4a2a1a",
+    "url": "/static/media/eligiblity.199806be.png"
+  },
+  {
+    "revision": "a204abfa8a09df48a7e49ed03fd1b672",
+    "url": "/static/media/footer-globe.a204abfa.png"
+  },
+  {
+    "revision": "c953559e48a24b9e1c12797f3305a2d0",
+    "url": "/static/media/hero-image.c953559e.png"
+  },
+  {
+    "revision": "38f68a2dd5b3ad7904c3ecf1f6499766",
+    "url": "/static/media/message.38f68a2d.png"
+  },
+  {
+    "revision": "e773669c0f1db1f598fc69553507fe54",
+    "url": "/static/media/mission.e773669c.png"
+  },
+  {
+    "revision": "b800edce3bae188f98c9161204180516",
+    "url": "/static/media/repayicon.b800edce.png"
+  },
+  {
+    "revision": "a14a95f09cb5837b72cdef343747b241",
+    "url": "/static/media/venus.a14a95f0.png"
+  },
+  {
+    "revision": "c212919dcd3165c4c6a665f6d2a54aab",
+    "url": "/static/media/vision.c212919d.png"
+  }
+]);
