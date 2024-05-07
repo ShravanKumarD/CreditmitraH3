fa871069375f25ce07e9b3ce633517da2bba8db5
ShravanKumarD <97720033+ShravanKumarD@users.noreply.github.com>
2024-05-07 15:43:39 +0530

    styling@homr-img

--- src/Pages/Blogs/CardComponent.js
+++ src/Pages/Blogs/CardComponent.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const CardComponent = ({ title, date, imageLink }) => (
-  <div className="feature-card">
-    <img src={imageLink} alt={title} />
-    <h3>{title}</h3>
-    <p>{date}</p>
+  <div className='row'>
+    <div className='col-md-4 blogcard'>
+      <div className='feature-card'>
+        <img src={imageLink} alt={title} />
+        <h5>{title}</h5>
+        <p>{date}</p>
+      </div>
+    </div>
   </div>
 );
 
