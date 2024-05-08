0fe531f79f060d64c299c458691d5a2202cbc89c
NafiGit <2100032542@kluniversity.in>
2024-05-08 15:57:03 +0530

    FAQ grammer changed

--- src/Pages/FAQ/FAQ.js
+++ src/Pages/FAQ/FAQ.js
@@ -10,22 +10,20 @@ function FAQ(props) {
     {
       General: [
         {
-          "What is Credit Mitra ?":
-           `CreditMitra is your go-to solution for all your financial needs. Whether it's timely EMI payments, fulfilling long-awaited purchases, planning a well-deserved vacation, or tackling unexpected emergencies, CreditMitra has you covered.`,
+          "What is Credit Mitra ?": `CreditMitra is your go-to solution for all your financial needs. Whether it's timely EMI payments, fulfilling long-awaited purchases, planning a well-deserved vacation, or tackling unexpected emergencies, CreditMitra has you covered.`,
         },
         {
-          "What is the Eligibility Criteria to apply for a loan?": 
-          `CreditMitra caters to salaried professionals aged 21 to 50 years. Qualify by maintaining a minimum net salary of Rs.15,000 with regular bank account credits for a seamless loan application process.`,
+          "What is the Eligibility Criteria to apply for a loan?": `CreditMitra caters to salaried professionals aged 21 to 50 years. Qualify by maintaining a minimum net salary of Rs.15,000 with regular bank account credits for a seamless loan application process.`,
         },
         {
-          "How does CreditMitra Works?": 
+          "How does CreditMitra Work?": 
           `CreditMitra is where simplicity meets convenience. All you need to do is follow
           through these steps to get an instant loan:
-          Download the CreditMitra app from Play Store.
-        ● Register effortlessly using Facebook, Gmail, or LinkedIn.
+          ● Download the CreditMitra app from Play Store.
+          ● Register effortlessly using Facebook, Gmail, or LinkedIn.
           ● Check eligibility with basic details.
           ● Complete the application with ease.
-          ● Upload documents’ photos conveniently.
+          ● Upload documents' photos conveniently.
           ● Add your bank account details securely.
           ●   Upon verification, expect swift fund transfer to your account.`,
         },
@@ -53,12 +51,11 @@ function FAQ(props) {
     {
       Technical: [
         {
-          "What happens if your platform crashes when I’m midway through my loan application process?":
-           " We have made sure that the loan request process on our platforms is smooth and bug-free. If you are using our website for your loan application, we recommend that you save your application frequently. And if your app crashes at any point in time, you will be able to start from the same step where the app crashed previously. Still  if you face any difficulty, please get in touch with us at support@creditmitra.in along with the details of the issue",
+          "What happens if your platform crashes when I'm midway through my loan application process?":
+            " We have made sure that the loan request process on our platforms is smooth and bug-free. If you are using our website for your loan application, we recommend that you save your application frequently. And if your app crashes at any point in time, you will be able to start from the same step where the app crashed previously. Still  if you face any difficulty, please get in touch with us at support@creditmitra.in along with the details of the issue",
         },
         {
-          "Why can’t I see the loans section on my Credit Mitra app whereas my friends can see such a section on theirs?": 
-          ` If you can’t see the Loans tab on your Credit Mitra app, please make sure to
+          "Why can't I see the loans section on my Credit Mitra app whereas my friends can see such a section on theirs?": ` If you can't see the Loans tab on your Credit Mitra app, please make sure to
           update the app to the latest version`,
         },
         {
@@ -76,8 +73,7 @@ function FAQ(props) {
     {
       Document: [
         {
-          ". What documents are required while applying for a Personal Loan from CreditMitra?":
-           ` PAN card
+          " What documents are required while applying for a Personal Loan from CreditMitra?": ` PAN card
            Latest salary slip from the same employer
            Present Address Proof – Aadhar/Electricity bill/Gas Bill/Telephone
            bill/Creditcard/Internet/HR Letter/PG Invoice
@@ -86,23 +82,20 @@ function FAQ(props) {
            `,
         },
         {
-         ". I am not registered for netbanking or if my netbanking ID is not valid, how can I send you my bank statement?":
-          ` You can provide us the last 3 months bank statement.`,
+          " I am not registered for netbanking or if my netbanking ID is not valid, how can I send you my bank statement?": ` You can provide us the last 3 months bank statement.`,
         },
         {
-          " I received/Downloaded the Bank Statement in Email from my Bank. However,when I am trying to upload it's not getting accepted?": 
-          ` Many times the bank statement sent or downloaded is not in the right format. To
+          " I received/Downloaded the Bank Statement in Email from my Bank. However,when I am trying to upload it's not getting accepted?": ` Many times the bank statement sent or downloaded is not in the right format. To
           ensure your loan is processed at the earliest, please download the bank statement in
           PDF from your bank website or the one received in your registered email id and
           upload it on our Credit Mitra app.`,
         },
-        {        
-  "Recently, I changed my bank account. It shows income/salary credit of only 1/2 months. Can I upload bank statements from both the banks to meet the 3 month bank statement requirement?": 
-  " You can reach out to us on support@creditmitra.in. We will be able to help you.",
+        {
+          "Recently, I changed my bank account. It shows income/salary credit of only 1/2 months. Can I upload bank statements from both the banks to meet the 3 month bank statement requirement?":
+            " You can reach out to us on support@creditmitra.in. We will be able to help you.",
         },
         {
-          " I am not able to upload docs online or see errors. Can I send the docs directly in email and you can process the application?": 
-          ` If you are facing problems in uploading the docs, you can reach out to us on
+          " I am not able to upload docs online or see errors. Can I send the docs directly in email and you can process the application?": ` If you are facing problems in uploading the docs, you can reach out to us on
           support@creditmitra.in. We will be able to help you.`,
         },
       ],
@@ -110,8 +103,7 @@ function FAQ(props) {
     {
       Eligibility: [
         {
-          "What are the eligibility criteria for Personal Loan?": 
-          ` You are eligible to apply personal loan on satisfying the following criteria –
+          "What are the eligibility criteria for Personal Loan?": ` You are eligible to apply personal loan on satisfying the following criteria –
           ● Should be an Indian citizen
           ● Should be a salaried employee
           ● Should have a minimum salary of Rs. 15,000/- p.m.
@@ -119,34 +111,28 @@ function FAQ(props) {
           ● Should be above 21yrs and not more than 50yrs. old`,
         },
         {
-          " How quickly will I get my Loan?": 
-          ` Your loan is generally transferred immediately as soon as all your documents are
+          " How quickly will I get my Loan?": ` Your loan is generally transferred immediately as soon as all your documents are
           verified. In order to process your loan quickly, we advise you to upload the necessary
           documents in the necessary formats and make sure they are clear and legible.`,
         },
         {
-          " What is the maximum and minimum loan amount that I can apply for?": 
-          ` The minimum amount that you can borrow on Credit Mitra loans is Rs.5,000. The
+          " What is the maximum and minimum loan amount that I can apply for?": ` The minimum amount that you can borrow on Credit Mitra loans is Rs.5,000. The
           maximum loan amount is calculated by our algorithm and can go up to Rs.2 Lakhs.`,
         },
         {
-          ". I am a self-employed individual, am I eligible for a loan from Credit Mitra?":
-          `No, Credit Mitra Personal loan is only for Salaried as of now.`,
+          " I am a self-employed individual, am I eligible for a loan from Credit Mitra?": `No, Credit Mitra Personal loan is only for Salaried as of now.`,
         },
         {
-          ". Will I get a loan if I get my salary in cash?": 
-          ` At this point in time, we only give loans to those individuals who get their salary
+          " Will I get a loan if I get my salary in cash?": ` At this point in time, we only give loans to those individuals who get their salary
           via bank transfer – directly from their employer. We will not be able to give loans to
           those individuals who get their salaries in cash.`,
         },
         {
-          " I have one loan running with Credit Mitra. Can I apply for another loan?":
-           ` No, we don’t provide the option of applying for a new loan if a user already has an
+          " I have one loan running with Credit Mitra. Can I apply for another loan?": ` No, we don't provide the option of applying for a new loan if a user already has an
            existing one.`,
-        }
+        },
       ],
     },
- 
   ];
   const [tabActive, setTabActive] = useState(Object.keys(faqContent[0])[0]);
   const [faqAnswerOpened, setFaqAnswerOpened] = useState({});
@@ -165,7 +151,12 @@ function FAQ(props) {
         {faqSelected.map((each, index) => {
           return (
             <div className="each-answer" key={index}>
-              <button onClick={() => { openfaqAnswer(index); }} className="btn faq-toggler">
+              <button
+                onClick={() => {
+                  openfaqAnswer(index);
+                }}
+                className="btn faq-toggler"
+              >
                 <div className="faq-toggler-content">
                   {Object.keys(each)[0]}
                   <svg
@@ -174,7 +165,8 @@ function FAQ(props) {
                     width="24"
                     height="15"
                     viewBox="0 0 24 15"
-                    fill="none">
+                    fill="none"
+                  >
                     <path
                       fillRule="evenodd"
                       clipRule="evenodd"
@@ -209,10 +201,8 @@ function FAQ(props) {
             <div className="content">
               <h6 className="heading">We Have Answers </h6>
               <div className="faq-container mx-auto">
-
-              <div className="input-box">
-
-{/* <input
+                <div className="input-box">
+                  {/* <input
 
     type="search"
 
@@ -227,21 +217,20 @@ function FAQ(props) {
     placeholder="Enter your search term here"
 
 /> */}
-
-</div>
+                </div>
 
                 <div className="faq-tab-container ">
                   <div className="tab-buttons row">
                     {faqContent.map((each, index) => {
                       return (
                         <div className="col-md-2">
-
                           <button
                             key={index}
-                            className={`${tabActive == Object.keys(each)[0]
+                            className={`${
+                              tabActive == Object.keys(each)[0]
                                 ? "active-btn"
                                 : ""
-                              } btn`}
+                            } btn`}
                             onClick={() => {
                               setTabActive(Object.keys(each)[0]);
                             }}
@@ -249,7 +238,6 @@ function FAQ(props) {
                             {Object.keys(each)[0]}
                           </button>
                         </div>
-
                       );
                     })}
                   </div>
