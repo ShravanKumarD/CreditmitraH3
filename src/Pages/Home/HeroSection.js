41c89dc380c0ab3ccdb57541b414dacd33227d78
NafiGit <2100032542@kluniversity.in>
2024-05-09 18:56:05 +0530

    btn brand wholly chnged, major style edit, buttons in home at end aligned well

--- src/Pages/Home/HeroSection.js
+++ src/Pages/Home/HeroSection.js
@@ -6,9 +6,9 @@ function HeroSection(props) {
             <div className='content a'>
                 <h6 className='hero-content'>Your Trusted Loan Mitra<br/>Loans Made Easy & Friendly</h6>
                 <p>At Credit mitra, we understand that life doesn't always wait for payday. That's why we're here to offer you instant personal loans with no hassle and no waiting. </p>
-                <div className='btn-container'>
+                <div className='hero-content-button'>
                     <buttton className="btn brand-primary py-3 px-4">Apply Now</buttton>
-                    <buttton className="btn brand-secondary  py-3 px-4">Download App</buttton>
+                    <buttton className="btn brand-secondary py-3 px-4">Download App</buttton>
                 </div>
             </div>
             <img src={heroImage} className='img-home'/>
