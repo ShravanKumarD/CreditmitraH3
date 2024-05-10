dec19495809e64b5ec6e80de38203cc947b997bb
ShravanKumarD <97720033+ShravanKumarD@users.noreply.github.com>
2024-05-10 13:18:49 +0530

    css

--- src/Pages/Blogs/CardComponent.js
+++ src/Pages/Blogs/CardComponent.js
@@ -2,13 +2,19 @@ import React from 'react';
 
 const CardComponent = ({ title, date, imageLink }) => (
   <div className='row'>
-    <div className='col-md-4 blogcard'>
-      <div className='feature-card'>
-        <img src={imageLink} alt={title} />
-        <h5>{title}</h5>
-        <p className='datefaq'>{date}</p>
+    <div>
+      <div className='col-sm-2'>
+        <div className='col-md-3 blogcard'>
+          <div className='feature-card'>
+            <img src={imageLink} alt={title} />
+            <h5>{title}</h5>
+            <p className='datefaq'>{date}</p>
+          </div>
+        </div>
+
       </div>
     </div>
+
   </div>
 );
 
