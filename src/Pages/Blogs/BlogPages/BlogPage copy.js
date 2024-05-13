5c79cb5e01441c83f19c0676e236bd3712e6e00e
NafiGit <2100032542@kluniversity.in>
2024-05-14 01:20:02 +0530

    added first blog with some styling

--- src/Pages/Blogs/BlogPages/BlogPage copy.js
+++ src/Pages/Blogs/BlogPages/BlogPage copy.js
@@ -4,7 +4,6 @@ import Header from "../../../Components/Header";
 import blogImage from "../../../assets/images/blogs/8_tips_to_improve_your_cibil_score.png";
 import SEO from "../../SEO/SEO";
 import "./BlogPage.css";
-import InstantLoanWithLowCibil from "./InstantLoanWithCibil";
 
 function Contact(props) {
   return (
@@ -22,18 +21,30 @@ function Contact(props) {
         <div className="page-container blog-content-page">
           <div className="blog-content-section">
             {/* we should decouple this image asap */}
-            <h1 className="blog-content-heading">How To Get An Instant Loans With Low CIBIL Score?</h1>
+            <h1 className="blog-content-heading">Title</h1>
             <div className="blog-content-date">May 14, 2024</div>
 
             <div className="blog-content-image">
               <img src={blogImage} className="blog-image-container" />
             </div>
             <div className="blog-content-text">
-              <InstantLoanWithLowCibil />
               <p className="blog-content-paragraph">
-
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
               </p>
               <p className="blog-content-paragraph">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
               </p>
             </div>
           </div>
