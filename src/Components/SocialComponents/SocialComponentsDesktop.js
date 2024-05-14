8f45399bcd4d0d118518af34f9d0658dd097be08
NafiGit <2100032542@kluniversity.in>
2024-05-14 12:08:16 +0530

    added twitter

--- src/Components/SocialComponents/SocialComponentsDesktop.js
+++ src/Components/SocialComponents/SocialComponentsDesktop.js
@@ -6,8 +6,13 @@ function SocialComponentsDesktop() {
     return (
         <div className='social-components-desktop'>
             <h3 style={{fontSize: '1.25rem', fontWeight: 'bold', marginBottom: '5%', marginTop: '11%', fontFamily: 'Poppins'}}>Follow Us</h3>
+            
+            <a href="https://twitter.com/CreditMitra/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter} size="2x" style={{marginRight: "5%", color: "#2B6CB0"}} /></a>
+
             <a href="https://www.instagram.com/creditmitra_official/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} size="2x" style={{marginRight: "5%", color: "#2B6CB0"}} /></a>
+
             <a href="https://www.facebook.com/profile.php?id=61554428303586" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebook} size="2x" style={{marginRight: "5%", color: "#2B6CB0"}} /></a>
+
             <a href="https://www.linkedin.com/company/creditmitra/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} size="2x" style={{marginRight: "5%", color: "#2B6CB0"}} /></a>
         </div>
     );
