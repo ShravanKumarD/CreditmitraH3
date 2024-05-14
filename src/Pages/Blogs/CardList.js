d812e9ae3fd19f431a59b63a390a7cead96b1ea1
NafiGit <2100032542@kluniversity.in>
2024-05-14 13:47:01 +0530

    added a link for blogs to take to their pages

--- src/Pages/Blogs/CardList.js
+++ src/Pages/Blogs/CardList.js
@@ -1,20 +1,34 @@
-  import React from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 
-  const CardList = ({ cards }) => (
-    <div className="featured-blog-container" style={{display:"flex", flexWrap: "wrap", justifyContent: "space-around"
-  }}>
-      {cards.map((card, index) => (
-        <div key={index} className="blog-item">
-          <div className="blog-card">
-            <div className="feature-card">
-              <img className="blog-feature-image" src={card.imageLink} alt={card.title} />
+
+const CardList = ({ cards }) => (
+  <div
+    className="featured-blog-container"
+    style={{
+      display: "flex",
+      flexWrap: "wrap",
+      justifyContent: "space-around",
+    }}
+  >
+    {cards.map((card, index) => (
+      <div key={index} className="blog-item">
+        <div className="blog-card">
+          <div className="feature-card">
+            <Link to={`/blogs/${card.blogSlug}`}>
+              <img
+                className="blog-feature-image"
+                src={card.imageLink}
+                alt={card.title}
+              />
               <h5 className="blog-feature-title">{card.title}</h5>
-              <p className="datefaq">{card.date}</p>
-            </div>
+            </Link>
+            <p className="datefaq">{card.date}</p>
           </div>
         </div>
-      ))}
-    </div>
-  );
+      </div>
+    ))}
+  </div>
+);
 
-  export default CardList;
+export default CardList;
