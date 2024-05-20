df02c70a47bdef5731de37d6e6a3d3ae0dd7a19c
NafiGit <2100032542@kluniversity.in>
2024-05-20 15:54:47 +0530

    fixed footer

--- src/Components/SocialComponents/SocialComponentsMobile.js
+++ src/Components/SocialComponents/SocialComponentsMobile.js
@@ -6,7 +6,7 @@ import { faXTwitter, faYoutube, faFacebook, faInstagram, faLinkedin } from '@for
 function SocialComponentsMobile() {
     return (
         <div className='social-components-mobile'>
-            <h3 style={{fontSize: '1.25rem', fontWeight: 'bold', marginBottom: '5%', marginTop: '11%', fontFamily: 'Poppins'}}>Follow Us</h3>
+            <h3 style={{fontSize: '1.25rem', fontWeight: 'bold', marginBottom: '5%', marginTop: '11%', fontFamily: 'Poppins', textAlign: 'left'}}>Follow Us</h3>
             <a href="https://twitter.com/CreditMitra/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faXTwitter} size="2x" style={{marginRight: "5%", color: "#2B6CB0"}} /></a>
             <a href="https://www.instagram.com/creditmitra_official/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} size="2x" style={{marginRight: "5%", color: "#2B6CB0"}} /></a>
             <a href="https://www.facebook.com/profile.php?id=61554428303586" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebook} size="2x" style={{marginRight: "5%", color: "#2B6CB0"}} /></a>
