cec7558572748c394807ff4905f7015f6fadeb98
NafiGit <2100032542@kluniversity.in>
2024-05-21 20:15:45 +0530

    margin in blogs (i think)

--- src/Pages/Blogs/FeaturedBlogs.js
+++ src/Pages/Blogs/FeaturedBlogs.js
@@ -61,7 +61,7 @@ function FeaturedBlogs(props) {
             <div className="content1">
               <h1
                 className="heading"
-                style={{ fontSize: "3rem", marginBottom: "5%",marginTop:"10%" }}
+                style={{ fontSize: "3rem", marginBottom: "5%",marginTop:"100px" }}
               >
                 Featured Blogs
               </h1>
