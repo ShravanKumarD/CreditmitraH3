370d74219a4e8e1cacd6b9b38d831f53d7fc8310
ShravanKumarD <97720033+ShravanKumarD@users.noreply.github.com>
2024-06-13 10:49:20 +0530

    added rcs

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -190,7 +190,7 @@ const handleChange = (e) => {
                   <div className="checkbox-container">
                     <input type="checkbox" id="notifications" name="notifications" value="accepted" required />
                     <label htmlFor="notifications" className="contactusCheckBox">
-                    I hereby authorize to receive notifications, SMS and informational emails
+                    I hereby authorize to receive notifications, SMS, RCS, and informational emails
                     </label>
                   </div>
 
