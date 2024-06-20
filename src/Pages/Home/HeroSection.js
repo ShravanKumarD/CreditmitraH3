2f08e91263c44d30899c246a82e72ea7a46cf5bb
NafiGit <2100032542@kluniversity.in>
2024-06-20 20:06:27 +0530

    redirect to /, samcinct

--- src/Pages/Home/HeroSection.js
+++ src/Pages/Home/HeroSection.js
@@ -1,19 +1,40 @@
-import React from 'react';
-import heroImage from '../../assets/images/hero-image1.png'
+import React from "react";
+import heroImage from "../../assets/images/hero-image1.png";
 function HeroSection(props) {
-    return (
-        <div className='hero-section'>
-            <div className='content a'>
-                <h6 className='hero-content'>Your Trusted Loan Mitra<br/>Loans Made Easy & Friendly</h6>
-                <p>At CreditMitra, we understand that life doesn't always wait for payday. That's why we're here to offer you instant personal loans with no hassle and no waiting. </p>
-                <div className='hero-content-button'>
-                    <buttton className="btn brand-primary py-3 px-4"onClick={() => window.location.href="https://samcintsolutions.uat.cloudbankin.com/onboard/#/login"}>Apply Now</buttton>
-                    <buttton className="btn brand-secondary py-3 px-4">Download App</buttton>
-                </div>
-            </div>
-            <img src={heroImage} className='img-home' alt='hero image' loading="lazy"/>
+  return (
+    <div className="hero-section">
+      <div className="content a">
+        <h6 className="hero-content">
+          Your Trusted Loan Mitra
+          <br />
+          Loans Made Easy & Friendly
+        </h6>
+        <p>
+          At CreditMitra, we understand that life doesn't always wait for
+          payday. That's why we're here to offer you instant personal loans with
+          no hassle and no waiting.{" "}
+        </p>
+        <div className="hero-content-button">
+          {/* apply now was directing to samcint, now it's redirecting to same page */}
+          <buttton
+            className="btn brand-primary py-3 px-4"
+            onClick={() => (window.location.href = "/")}
+          >
+            Apply Now
+          </buttton>
+          <buttton className="btn brand-secondary py-3 px-4">
+            Download App
+          </buttton>
         </div>
-    );
+      </div>
+      <img
+        src={heroImage}
+        className="img-home"
+        alt="hero image"
+        loading="lazy"
+      />
+    </div>
+  );
 }
 
 export default HeroSection;
